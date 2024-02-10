ba61e03e68f006233dd43d73c03638496584c29b
Vocal <vocalnight@protonmail.com>
2024-02-09 23:07:29 -0300

    added db, migrations and started the minmal API config

---
+++ ShoppingListReact/Models/ShoppingListContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShoppingListReact.Models
+{
+    public class ShoppingListContext: DbContext
+    {
+        public ShoppingListContext( DbContextOptions<ShoppingListContext> options ) : base(options) { }
+
+        public DbSet<Item> ShoppingListItems { get; set; }
+    }
+}
